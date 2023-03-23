c5a272892cba6c4e090e648ad0d8910218a14c7a
Ismael Bueno <ismaelwno@gmail.com>
2023-03-24 00:28:38 +0100

    Added premake5 file

---
+++ premake5.lua
@@ -0,0 +1,13 @@
+project "SFML"
+   language "C++"
+   cppdialect "C++20"
+   staticruntime "on"
+   kind "StaticLib"
+
+   targetdir("../bin/" .. outputdir)
+   objdir("../build/" .. outputdir .. "/%{prj.name}")
+
+   files {"src/**.cpp", "include/**.hpp"}
+
+   includedirs {"../**/include", "extlibs/headers", "extlibs/headers/**"}
+   removeincludedirs "extlibs/headers/FLAC"
